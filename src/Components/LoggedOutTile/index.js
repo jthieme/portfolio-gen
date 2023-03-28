97956310a6b503bbb70d81144bcde30506b835bb
Joshua Thieme <jomthieme@gmail.com>
2023-03-27 21:56:12 -0600

    got the logged out home page to match designs

---
+++ src/Components/LoggedOutTile/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const LoggedOutTile = ({ title, content }) => {
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "#40434E",
+          width: "18%",
+          paddingLeft: "1%",
+          paddingRight: "1%",
+          paddingBottom: "5%",
+          paddingTop: "1%",
+          borderRadius: "10%",
+          display: "inline-block",
+          marginLeft: "5%",
+              
+        }}
+      >
+        <h3 style={{ textAlign: "center", marginBottom: "-8%", marginTop: "-1%" }}>{title}</h3>
+        <div className="divider text-white" />
+        <h5 >{content}</h5>
+      </div>
+
+
+
+      {/* <div style={{ padding: "2%", display: "inline-block" }}>
+        <div
+          class="card h-100p u-flex u-flex-column text-white"
+          style={{
+            maxWidth: "300px",
+            padding: "8%",
+            backgroundColor: "#40434E",
+          }}
+        >
+          <div class="content u-text-center pt-3">
+            <h3 id="projectname" class="title mt-2 mb-0">
+              {title}
+            </h3>
+            <div className="divider text-white" />
+            <h4>{content}</h4>
+          </div>
+        </div>
+      </div> */}
+    </>
+  );
+};
+
+export default LoggedOutTile;
--- src/Pages/Home/DefaultHome.js
+++ src/Pages/Home/DefaultHome.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import globe from '../../assets/globe.jpg';
+import globe from "../../assets/globe.jpg";
+import LoggedOutTile from "../../Components/LoggedOutTile";
 
 const largeDiv = styled.div`
   display: "flex",
@@ -12,9 +13,46 @@ const largeDiv = styled.div`
 const DefaultHome = () => {
   return (
     // <largeDiv>
-      <div style={{marginTop: "-1.3%", height: "442px"}}>
-        <img src={globe} style={{width: "268.5%", height: "inherit"}}/>
+    <div>
+      <div style={{ marginTop: "-1.3%", height: "442px" }}>
+        <img src={globe} style={{ width: "268.5%", height: "inherit" }} />
+        <div style={{ marginTop: "-18%", display: "flex", marginLeft: "20%" }}>
+          <h1
+            className="text-white font-alt font-bold"
+            style={{ textAlign: "center" }}
+          >
+            Sign up today. Get hired tomorrow.
+          </h1>
+          <button
+            className="bg-orange-500 text-white"
+            style={{ marginLeft: "3%" }}
+          >
+            Sign Up
+          </button>
+        </div>
       </div>
+      <div style={{ backgroundColor: "#1e1e1e" }} className="text-white">
+        <h2 style={{ textAlign: "center", paddingTop: "2%" }}>What We Do</h2>
+        <div style={{ padding: "5%", paddingLeft: "6%", display: "flex" }}>
+          <LoggedOutTile
+            title={"Content"}
+            content={"Sign in with your GitHub account."}
+          />
+          <LoggedOutTile
+            title={"Select"}
+            content={"Choose exactly which repositories you want to showcase."}
+          />
+          <LoggedOutTile
+            title={"Edit"}
+            content={"Edit every property you can think of to make your Portfolio yours!"}
+          />
+          <LoggedOutTile
+            title={"Share"}
+            content={"When you’re done, Publish your portfolio for employers to see."}
+          />
+        </div>
+      </div>
+    </div>
     // </largeDiv>
   );
 };
